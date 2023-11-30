38d0d1f44a505333d07a628395cdcc8e65aa3845
Ciprian <cipri167@gmail.com>
2023-11-30 23:06:52 +0200

    test implementation

--- src/test/java/stepdefinitions/StepDefinitions.java
+++ src/test/java/stepdefinitions/StepDefinitions.java
@@ -6,14 +6,21 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.java.After;
 import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.FindBy;
 import pageobjects.Homepage;
 import pageobjects.ResultsPage;
 
 
 import java.io.File;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class StepDefinitions {
 
@@ -21,8 +28,10 @@ public class StepDefinitions {
 
     public void startDriver(String url){
 
-        System.setProperty("webdriver.chrome.driver", new File("chromedriver.exe").getPath());
-        driver = new ChromeDriver();
+        ChromeOptions co = new ChromeOptions();
+        co.addArguments("--remote-allow-origins=*");
+        System.setProperty("webdriver.chrome.driver", new File("src/test/resources/drivers/chromedriver.exe").getPath());
+        driver = new ChromeDriver(co);
         driver.get(url);
         driver.manage().window().maximize();
 
@@ -48,21 +57,47 @@ public class StepDefinitions {
     }
 
     @And("I enter flight from {string} to {string}")
-    public void iEnterFlightFromTo(String string1, String string2) {
+    public void iEnterFlightFromTo(String string1, String string2) throws InterruptedException {
         Homepage homepage = new Homepage(driver);
         homepage.flyFromInput.sendKeys(string1);
+        Thread.sleep(500);
+        homepage.flyFromInput.sendKeys(Keys.TAB);
         homepage.flyToInput.sendKeys(string2);
+        Thread.sleep(500);
+        homepage.flyToInput.sendKeys(Keys.TAB);
     }
 
     @And("I select as a departing date the next day and returning after {int} days")
     public void iSelectAsADepartingDateTheNextDayAndReturningAfterDays(int days) {
         Homepage homepage = new Homepage(driver);
         homepage.departingDatePicker.click();
-        String departingDate = homepage.departingDatePicker.getAttribute("value");
+        LocalDate currentDate = LocalDate.now();
+        LocalDate tomorrowDate = currentDate.plusDays(1);
 
-        LocalDateTime today = new LocalDateTime.now();
-        LocalDateTime tomorrow = today.plusDays(1);
-        LocalDateTime futureDate = today.plusDays(days);
+        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        String formattedCurrentDate = tomorrowDate.format(inputFormatter);
+
+        LocalDate date = LocalDate.parse(formattedCurrentDate, inputFormatter);
+
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("MMMM d, yyyy");
+
+        String formattedTomorrowDate = date.format(outputFormatter);
+        String xPathExpression = "//td[contains(@aria-label,'" + formattedTomorrowDate + "')]";
+
+        LocalDate futureDate = tomorrowDate.plusDays(days);
+
+        String formattedFutureDate = futureDate.format(inputFormatter);
+
+        LocalDate fDate = LocalDate.parse(formattedFutureDate, inputFormatter);
+
+        String formattedFDate = fDate.format(outputFormatter);
+        String fXPathExpression = "//td[contains(@aria-label,'" + formattedFDate + "')]";
+
+        WebElement tomorrowDatePicker = driver.findElement(By.xpath(xPathExpression));
+        tomorrowDatePicker.click();
+        WebElement futureDatePicKer = driver.findElement(By.xpath(fXPathExpression));
+        futureDatePicKer.click();
     }
 
     @When("I click find deal button")
